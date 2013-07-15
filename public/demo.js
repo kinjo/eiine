e5047d193f400b3f4814a88ae72883230a2553c7
Takumi KINJO <takumi.kinjo@gmail.com>
2013-07-16 08:26:52 +0900

    revert demo.js

--- public/demo.js
+++ public/demo.js
@@ -1,8 +1,6 @@
 var GL,FB;
 function start(effectClass){
   var flag=false;
-  document.body.onclick=function(){flag=true;}
-  document.body.onkeydown=function(){flag=true;}
   var canvas=document.getElementById("webglcanvas");
   GL=canvas.getContext("experimental-webgl");
   GL.framebuffer=new FrameBufferObject();
@@ -14,11 +12,48 @@ function start(effectClass){
   var effect = new effectClass();
   effect.render(renderTarget,1);
   var t=new Date();
+
+  var pre_update='';
+  var iinen=0; // number of iine
+  var ws = new WebSocket('ws://' + $('#hostname').val());
+  var ws_open=false;
+  ws.onopen = function () {
+    ws_open=true;
+  };
+  ws.onmessage = function (e) {
+    eval('var json=' + e.data);
+    if(pre_update != json.update){
+      flag=true;
+      pre_update=json.update;
+      iinen+=json.count;
+    }
+  };
+  var iinef = function(){
+    $.ajax({
+      type:'POST',
+      url:'/',
+      data:{session_id:$('#session_id').val()},
+      success:function(){
+        ws.send('message');
+      }
+    });
+  };
+  $('body').click(iinef);
+  //$('body').click(iinef).keydown(iinef);
+
+  setInterval(function(){
+    ws.send('message');
+  },500);
   setInterval(function(){
     if(flag&&(new Date()-1000/10>t)){
-      effect.render(renderTarget,1);
+      // reander the iine
+      for (var i=0;i<iinen;i++) {
+        effect.render(renderTarget,1);
+      }
+      iinen=0; // clear number of iine
       flag=false;
       t=new Date();
     }else effect.render(renderTarget,0);
-  },10)
+  },10);
 }
+
